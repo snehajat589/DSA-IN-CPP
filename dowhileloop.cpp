345da2534424df60a7ce0a3b58482318650f5ae1
snehajat589 <snehajat542@gmail.com>
2026-07-25 00:13:44 +0530

    first commit

---
+++ dowhileloop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i = 1;
+    cin >> n;
+
+    do {
+        cout << i << " ";
+        i++;
+    } while(i <= n);
+
+    return 0;
+}
