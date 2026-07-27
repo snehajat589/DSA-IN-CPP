4105a1aa169123b354a7a06986d3353801cb9d20
snehajat589 <snehajat542@gmail.com>
2026-07-27 23:31:31 +0530

    first commit

---
+++ nestedlopp.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    #include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j;
+
+    for(i = 1; i <= 3; i++)
+    {
+        for(j = 1; j <= 3; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+}
