92f82b6061fdf266db8b59d3762e51f256a81952
snehajat589 <snehajat542@gmail.com>
2026-07-29 22:52:18 +0530

    first commit

---
+++ string.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace  std;
+int main(){
+    string firstname = "sneha";
+    string lastname = "jat";
+    cout<<firstname<<endl;
+    cout<<lastname<<endl;
+    return 0;
+}
